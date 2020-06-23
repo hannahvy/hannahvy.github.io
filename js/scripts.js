a9efcfe3704e05d3e2e6d793108f1bba1c82f539
hannahvy <52050834+hannahvy@users.noreply.github.com>
2020-06-23 19:15:42 -0400

    f

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
-    new TypeIt("#p", {
+    new TypeIt("#simpleUsage", {
       strings: "I'm a UX Design and Computer Science Student.",
       speed: 75,
       loop: true
